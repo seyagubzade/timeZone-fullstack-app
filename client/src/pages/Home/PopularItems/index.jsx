53aa3ef2320fd0f8b75535952e5653d762d1a357
Sevinj Yagubzade <sevinjyagubzade@Sevinjs-wifi.local>
2024-01-20 12:21:13 +0400

    filter fixed

--- client/src/pages/Home/PopularItems/index.jsx
+++ client/src/pages/Home/PopularItems/index.jsx
@@ -20,7 +20,7 @@ const PopularItems = () => {
   useEffect(() => {
     setFilteredData(() => {
       return data.filter((item) =>
-        item.name?.toLowerCase().includes(searchValue.toLowerCase().trim())
+        item.name.toLowerCase().includes(searchValue.toLowerCase().trim())
       );
     });
   }, [data, searchValue]);
@@ -35,10 +35,77 @@ const PopularItems = () => {
             gravida.
           </p>
         </div>
+        <div className="filter-section row justify-content-between">
+          <div class="form-group col-12 col-md-6 col-lg-6 mt-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Search"
+              value={searchValue}
+              onChange={(e) => {
+                setSearchValue(() => e.target.value);
+              }}
+            />
+          </div>
+          <div class="btn-group col-12 col-md-6 col-lg-6 mt-3" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              class="btn btn-light"
+              onClick={() => setFilteredData(data)}
+            >
+              Default
+            </button>
+            <button
+              type="button"
+              class="btn btn-light"
+              onClick={() =>
+                setFilteredData(() => {
+                  return [...data].sort((a, b) => a.price - b.price);
+                })
+              }
+            >
+              Lowest to Highest
+            </button>
+            <button
+              type="button"
+              class="btn btn-light"
+              onClick={() =>
+                setFilteredData(() => {
+                  return [...data].sort((a, b) => b.price - a.price);
+                })
+              }
+            >
+              Highest to Lowest
+            </button>
+            <button
+              type="button"
+              class="btn btn-light"
+              onClick={() =>
+                setFilteredData(() => {
+                  return [...data].sort((a, b) => a.name.localeCompare(b.name));
+                })
+              }
+            >
+              A to Z
+            </button>
+            <button
+              type="button"
+              class="btn btn-light"
+              onClick={() =>
+                setFilteredData(() => {
+                  return [...data].sort((a, b) => b.name.localeCompare(a.name));
+                })
+              }
+            >
+              Z to A
+            </button>
+          </div>
+        </div>
+
         <div className="row remove-margin">
           {loading ? (
             <p>Loading</p>
-          ) : data ? (
+          ) : filteredData ? (
             filteredData?.map((item) => (
               <div className="col-12 col-md-6 col-lg-4">
                 <div className="item-card mt-5">
@@ -48,16 +115,24 @@ const PopularItems = () => {
                     style={{ width: "100%", height: "auto" }}
                   />
                   <Link to={`/detail/${item._id}`}>
-                    <h3 className="title text-center mt-4">
-                      Thermo Ball Etip Gloves
-                    </h3>
+                    <h3 className="title text-center mt-4">{item.name}</h3>
                   </Link>
-                  <p className="price text-center text-danger">$ 43.12</p>
+                  <p className="price text-center text-danger">
+                    $ {item.price}
+                  </p>
                   <div className="actions text-center">
-                    <button className="btn btn-outline-dark" onClick={()=>dispatch(addToWishlist(item))}>
+                    <button
+                      className="btn btn-outline-dark"
+                      onClick={() => dispatch(addToWishlist(item))}
+                    >
                       Add to wishlist
                     </button>{" "}
-                    <button className="btn btn-dark" onClick={()=>dispatch(addToBasket(item))}>Add to basket</button>
+                    <button
+                      className="btn btn-dark"
+                      onClick={() => dispatch(addToBasket(item))}
+                    >
+                      Add to basket
+                    </button>
                   </div>
                 </div>
               </div>
